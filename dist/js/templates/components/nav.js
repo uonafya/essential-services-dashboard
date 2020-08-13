7cf52c7ff670c9cc94d2268d210cef94e3d2dfeb
Benzer Bett <benzerbett@gmail.com>
2020-08-13 23:50:01 +0300

    non-comminicable - 2 visualizations pending endpoints only

--- dist/js/templates/components/nav.js
+++ dist/js/templates/components/nav.js
@@ -25,15 +25,26 @@ const nav_template = `
             <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="rmncah-vitasup.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">Vitamin A supplementation</span></a></li>
         </ul>
     </li>
-<li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu">Communicable Diseases </span></a>
-    <ul aria-expanded="false" class="collapse  first-level">
-        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="communicable-hiv.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">HIV</span></a></li>
+    <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu">Communicable Diseases </span></a>
+        <ul aria-expanded="false" class="collapse  first-level">
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="communicable-hiv.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">HIV</span></a></li>
 
-        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="communicable-tb.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">Tuberculosis</span></a></li>
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="communicable-tb.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">Tuberculosis</span></a></li>
 
-        <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="communicable-malaria.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">Malaria</span></a></li>
-    </ul>
-</li>
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="communicable-malaria.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">Malaria</span></a></li>
+        </ul>
+    </li>
+    <li class="sidebar-item"> <a class="sidebar-link has-arrow waves-effect waves-dark" href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-receipt"></i><span class="hide-menu">Non-Communicable Diseases </span></a>
+        <ul aria-expanded="false" class="collapse  first-level">
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="non-communicable-cancer.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">Cancer</span></a></li>
+
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="non-communicable-diabetes.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">Diabetes</span></a></li>
+
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="non-communicable-hypertension.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">Hypertension</span></a></li>
+
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="non-communicable-kidney.html" aria-expanded="false"><i class="mdi mdi-border-inside"></i><span class="hide-menu">Kidney disease</span></a></li>
+        </ul>
+    </li>
     <li class="sidebar-item hidden">
         <a class="sidebar-link" href="birthsdeathsreg.html" aria-expanded="false">
             <small class="text-muted text-smaller"><i class="fa-small fa fa-th fcwhite hidden"></i></small>&nbsp;
