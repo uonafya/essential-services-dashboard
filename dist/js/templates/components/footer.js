f3a90b714722cb2b27c3f19ea238afe7a4e3805b
Benzer Bett <benzerbett@gmail.com>
2020-11-23 13:00:35 +0300

    logo placements, 1.0.28

--- dist/js/templates/components/footer.js
+++ dist/js/templates/components/footer.js
@@ -1,7 +1,26 @@
 const footer_template = `
 <footer class="footer text-center">
-    All Rights Reserved. Essential Services Report &copy;
-    {{yr}}
+    <div class="row">
+        <div class="col-md-4 text-center"></div>
+        <div class="col-md-3 text-center">
+            <p class="m-t-10">All Rights Reserved. Ministry of Health, Government of Kenya. &copy;
+            {{yr}}</p>
+        </div>
+        <div class="col-md-5">
+            <ul class="list-inline">
+                <li class="list-inline-item">
+                    <a href="https://usaid.gov/kenya/global-health/" class="bcwhite ws-nowrap bdr-3 p-3 m-r-5">
+                        <img src="../assets/images/logos/USAID-Kenya.png" alt="USAID Kenya" style="max-height: 37px"/>
+                    </a>
+                </li>
+                <li class="list-inline-item">
+                    <a href="https://healthit.uonbi.ac.ke" class="bcwhite ws-nowrap bdr-3 p-3">
+                        <img src="../assets/images/logos/healthit.png" alt="HealthIT" style="max-height: 37px"/>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
 </footer>
 `
 
